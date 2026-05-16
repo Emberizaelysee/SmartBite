1a9734f3a64b7beda0aad9034e6b2a78e5580e29
Marianne Dagher <marianne.dagher@isae.edu.lb>
2026-05-16 17:41:11 +0300

    update purchase-function.php

--- Backend/config/connection.php
+++ Backend/config/connection.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "smartbitetest";
+$db = "smartbite";
 $conn = new mysqli($host,$user,$pass,$db);
 if($conn->connect_error){
     die("connection failed".$conn->connect_error);
